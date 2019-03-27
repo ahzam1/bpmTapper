c62a7722b91cc742221a2c63c47c070d06d8787e
ahzam1 <ahzam1@ualberta.ca>
2019-03-27 02:03:55 -0600

    some more work and cleaning up!

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-console.log("test")
+
 let clicks = 0;
 let playing = false;
 let now = 0;
+let bpmSaved=0;
+let bpmArray = [];
 let lastclick = 0;
-const displayBPM = document.getElementById('bpm');
+const savebutton = document.getElementById('save');
 
 const tapper = document.getElementById('tapper')
-
+$(document).ready(function(){
+  //anything we need to do when ready?
+})
 
 tapper.addEventListener('click', function() {
   if (clicks == 0) {
@@ -19,7 +23,8 @@ tapper.addEventListener('click', function() {
   if (seconds - lastclick > 3) {
     clicks = 1;
     now = new Date().getTime();
-    displayBPM.innerHTML = "resetting...";
+    tapper.innerHTML = "resetting...";
+    $('#save').css("visibility", "hidden");
   }
 
   lastclick = seconds;
@@ -27,6 +32,38 @@ tapper.addEventListener('click', function() {
   var bpm = (clicks / minutes).toFixed(1);
   clicks++;
   if (clicks > 5) {
-    displayBPM.innerHTML = bpm + " BPM";
+    tapper.innerHTML = bpm + " BPM";
+    $('#save').css("visibility", "visible");
   }
 })
+
+
+savebutton.addEventListener('click', function() {
+  var str = tapper.innerHTML.slice(0, -4);
+  if(!bpmArray.includes(str)){
+    bpmArray.push(str);
+  }
+  $('#savedbpm').css("visibility", "visible");
+  $('#savedbpm').empty();
+  document.getElementById('savedbpm').appendChild(makeUL(bpmArray));
+})
+
+
+function makeUL(array) {
+    // Create the list element:
+    var list = document.createElement('ul');
+
+    for(var i = 0; i < array.length; i++) {
+        // Create the list item:
+        var item = document.createElement('li');
+
+        // Set its contents:
+        item.appendChild(document.createTextNode(array[i] + " BPM"));
+
+        // Add it to the list:
+        list.appendChild(item);
+    }
+
+    // Finally, return the constructed list:
+    return list;
+}
